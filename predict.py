ae76781222722e3771b1cb7c18d31e32247cc9fb
Caio Ferreira Bernardo <caio.bernardo@usp.br>
2021-01-27 08:57:36 -0300

    Add utilities
    Add utilities "select_classes", that returns a subset of the dataset according to the classes choosen, and "write_results", that write the results of the experiment to a .txt file. Also implement a full test of a simple cnn

--- predict.py
+++ predict.py
@@ -30,13 +30,44 @@ import math
 from model import Model
 import utilities
 
+# 0. Parameters
+epochs = 15
+filename = "full_test_simple_cnn.txt"
+
 # 1. Load images
 X, y, class_names = utilities.load_images()
 
+X_an_av, y_an_av, class_names_an_av = utilities.select_classes(X, y, [0,1], class_names)
+X_an_in, y_an_in, class_names_an_in = utilities.select_classes(X, y, [0,2], class_names)
+X_av_in, y_av_in, class_names_av_in = utilities.select_classes(X, y, [1,2], class_names)
+
+
 # 2. Load model
-model = Model(model_name="simple_cnn", epochs=2)
+model_name = "simple_cnn"
+model = Model(model_name=model_name, epochs=epochs, n_classes=3)
+model_an_av = Model(model_name=model_name, epochs=epochs, n_classes=2)
+model_an_in = Model(model_name=model_name, epochs=epochs, n_classes=2)
+model_av_in = Model(model_name=model_name, epochs=epochs, n_classes=2)
 
 # 3. train model
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+X_train_an_av, X_test_an_av, y_train_an_av, y_test_an_av = train_test_split(X_an_av, y_an_av, test_size=0.1, random_state=42)
+X_train_an_in, X_test_an_in, y_train_an_in, y_test_an_in = train_test_split(X_an_in, y_an_in, test_size=0.1, random_state=42)
+X_train_av_in, X_test_av_in, y_train_av_in, y_test_av_in = train_test_split(X_av_in, y_av_in, test_size=0.1, random_state=42)
 
-model.skf_fit(X_train, y_train, X_test, y_test, save_model=True, verbose=True)
+
+acc_list, mean_acc, std_acc, loss_list, mean_loss, std_loss, best_model = model.skf_fit(X_train, y_train, X_test, y_test, save_model=True, verbose=True)
+acc_list_an_av, mean_acc_an_av, std_acc_an_av, loss_list_an_av, mean_loss_an_av, std_loss_an_av, best_model_an_av = model_an_av.skf_fit(X_train_an_av, 
+																					y_train_an_av, X_test_an_av, y_test_an_av, save_model=True, verbose=True)
+acc_list_an_in, mean_acc_an_in, std_acc_an_in, loss_list_an_in, mean_loss_an_in, std_loss_an_in, best_model_an_in = model_an_in.skf_fit(X_train_an_in, 
+																					y_train_an_in, X_test_an_in, y_test_an_in, save_model=True, verbose=True)
+acc_list_av_in, mean_acc_av_in, std_acc_av_in, loss_list_av_in, mean_loss_av_in, std_loss_av_in, best_model_av_in = model_av_in.skf_fit(X_train_av_in, 
+																					y_train_av_in, X_test_av_in, y_test_av_in, save_model=True, verbose=True)
+
+
+# 4. Write results
+
+utilities.write_results(filename, class_names, acc_list, loss_list, model_name)
+utilities.write_results(filename, class_names_an_av, acc_list_an_av, loss_list_an_av, model_name)
+utilities.write_results(filename, class_names_an_in, acc_list_an_in, loss_list_an_in, model_name)
+utilities.write_results(filename, class_names_av_in, acc_list_av_in, loss_list_av_in, model_name)
--- utilities.py
+++ utilities.py
@@ -27,3 +27,33 @@ def load_images():
 	y = np.array(y)
 
 	return X, y, class_names
+
+def select_classes(X, y, classes, class_names):
+	select_class_names = np.array(class_names)[np.array(classes)-1].tolist()
+
+	indexes = np.array([])
+	for class_id in classes:
+		indexes = np.append(indexes, np.where(y[:] == class_id))
+
+	y_selected = y[indexes.astype(int)] - (y[indexes.astype(int)] / len(classes)).astype(int)
+	X_selected = X[indexes.astype(int)]
+
+	return X_selected, y_selected, select_class_names
+
+def write_results(filename, classes, acc_list, loss_list, model_name):
+	f = open(filename, "a+")
+
+	mean_acc = np.mean(acc_list)
+	std_acc = np.std(acc_list)
+
+	mean_loss = np.mean(loss_list)
+	std_loss = np.std(loss_list)
+
+	f.write("\n===========================================================================================================\n")
+	f.write(f"Model: {model_name}\nClasses: {classes}")
+	f.write("\n\n\t\t\t\t| Value\t\t\t\t\tstd_dev\n")
+	f.write("-------------------------------------------------------------------\n")
+	f.write(f"Mean Accuracy\t| {mean_acc}\t(+/- {std_acc})\n")
+	f.write(f"Mean Loss\t\t| {mean_loss}\t(+/- {std_loss})\n")
+
+
